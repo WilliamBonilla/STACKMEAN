4f99335c822bf06b32e8bc26db43086ebb939962
William Bonilla <w.bonillasw@gmail.com>
2014-12-28 10:48:50 -0600

    Desarrollo STACK MEAN

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var app = express();
 app.use(express.Router());
 app.use(express.static(__dirname + '/public'));
+var terminal = require('color-terminal');
 
 app.get('/', function(req, res){
    res.sendFile('public/index.html');
@@ -13,6 +14,10 @@ var server = app.listen(3000, function () {
   var host = server.address().ip;
   var port = server.address().port;
 // || "127.0.0.1";
-  console.log('Application listen on 127.0.0.1 PORT %s',port);
-
+  terminal.colorize('%_ %r').write('\nNinja Web Corporation S.A de C.V');
+  terminal.colorize('%G').write('\nDesarrollo con STACK MEAN');
+  terminal.colorize('%N');
+  console.log('\n\nApplication listen on 127.0.0.1 PORT: %s',port);
+  terminal.colorize('%_ %p').write('\nPress Ctrl+C to Stop Server\n\n');
+  terminal.colorize('%N');
 });
