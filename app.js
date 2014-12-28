7e951cfd4c810152b1330ba5bfe37fd379338689
Chiliseo <adrianochiliseo@hotmail.com>
2014-12-28 12:49:41 -0600

    MongoDB Mongoose

--- app.js
+++ app.js
@@ -4,11 +4,85 @@ var app = express();
 app.use(express.Router());
 app.use(express.static(__dirname + '/public'));
 var terminal = require('color-terminal');
+var clienteJs = require('./lib/db.js')
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/personas', function(error){
+   if(error){
+      throw error; 
+   }else{
+      console.log('Conectado a MongoDB');
+   }
+});
+var Cliente = new clienteJs(mongoose);
 
 app.get('/', function(req, res){
-   res.sendFile('public/index.html');
+   Cliente.find({}, function(error, clientes){
+      if(error){
+         res.send('Error.');
+      }else{
+         res.send(clientes);
+      }
+   })
 });
-
+app.get('/recuperar', function(req, res){
+   Cliente.findById(req.query._id, function(error, documento){
+      if(error){
+         res.send('Error.');
+      }else{
+         res.send(documento);
+      }
+   });
+});
+app.post('/guardar', function(req, res){
+   if(req.query._id == null){
+      //Inserta
+      var cliente = new Cliente({
+         nombre: req.query.nombre,
+         apellido: req.query.apellido,
+         domicilio: req.query.domicilio,
+         telefono: req.query.telefono,
+         email: req.query.email
+      });
+      cliente.save(function(error, documento){
+         if(error){
+            res.send('Error.');
+         }else{
+            res.send(documento);
+         }
+      });
+   }else{
+      //Modifica
+      Cliente.findById(req.query._id, function(error, documento){
+         if(error){
+            res.send('Error al intentar modificar el personaje.');
+         }else{
+            var cliente = documento;
+            cliente.nombre = req.query.nombre,
+            cliente.apellido = req.query.apellido,
+            cliente.domicilio = req.query.domicilio,
+            cliente.telefono = req.query.telefono,
+            cliente.email = req.query.email
+            cliente.save(function(error, documento){
+               if(error){
+                  res.send('Error.');
+               }else{ 
+                  res.send(documento);
+               }
+            });
+         }
+      });
+   }
+});
+app.post('/eliminar', function(req, res){
+   Cliente.remove({_id: req.query._id}, function(error){
+      if(error){
+         res.send('Error.');
+      }else{
+         res.send('Ok');
+      }
+   });
+});
 var server = app.listen(3000, function () {
 
   var host = server.address().ip;
